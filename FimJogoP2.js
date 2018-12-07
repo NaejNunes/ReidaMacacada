f34c99f244b408628b28890094e32fa4699bde28
Jean Nunes <jeannobreganunes@hotmail.com>
2018-12-07 09:09:20 -0200

    Atualizado cena de vencedor

--- FimJogoP2.js
+++ FimJogoP2.js
@@ -1,48 +1,33 @@
 var game = new Phaser.Game(
 
-	400, 300, Phaser.AUTO, '',
+	800, 600, Phaser.AUTO, '',
 	{
-		
 		preload: preload,
 		create: create,
 		update: update
-		
 	}
 );
-
- var vencedortxt;
- var Macaco;
- var Aplausos;
-
-// Função que faz o pre carregamento dos sprites
 function preload()
 {
-	game.load.image('player2', 'assets/Player2Vencedor.png');
-	game.load.audio('macaco', 'assets/Sons/MacacoGritando.mp3');
-	game.load.audio('aplausos', 'assets/Sons/Aplausos.mp3');
-	
-	game.load.spritesheet('ButtonPlay', 'assets/AnimButton.png', 62, 60);
+	game.load.spritesheet('ButtonPlay', 'assets/Botoespequenos.png', 105, 100);
+	game.load.image('background', 'assets/Bkvencedorp2.png');
 }
-			
+
+var button;
+var background; 
+	
 function create()
 {
+	background = game.add.tileSprite(0, 0, 800, 600, 'background');
 	
-	
-	
-	//Instacia sprite de macaco vencedor
-	game.add.sprite(150,150, 'player2');
-	vencedortxt = game.add.text(70,100, 'O Rei da Macacada',{'fontSize': '30px', 'fill': '#ffffff'});
-	
-	//Som de aplausos e macaco
-	Macaco = game.sound.play('macaco');
-	Aplausos = game.sound.play('aplausos');
-	
-	button = game.add.button(300, 220, 'ButtonPlay', actionOnClick, this, 0, 1, 2);
+	button = game.add.button(600, 480, 'ButtonPlay', actionOnClick, this, 1, 2, 0);
 	
 	button.onInputOver.add(over, this);
 	button.onInputOut.add(out, this);
-	button.onInputUp.add(up, this); 
-}
+	button.onInputUp.add(up, this);
+	
+}	
+
 function Over()
 {
 	console.log('button over', arguments);
@@ -59,9 +44,9 @@ function up()
 function actionOnClick()
 {
 	window.location.replace("playgame.html");	
-}				
+}
+					
 function update()
 {
-		
+
 }
-		
